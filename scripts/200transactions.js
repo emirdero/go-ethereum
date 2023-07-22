a5dd176d490064e13506acd6b881896da1bb2f72
Emir Derouiche <memir0@hotmail.no>
2023-07-22 17:05:35 +0200

    add transaction scripts

---
+++ scripts/200transactions.js
@@ -0,0 +1,6 @@
+for(let i = 0; i < 200; i++){
+    eth.sendTransaction({
+        from: eth.accounts[0],
+        to: "0xceee57f2b700c2f37d1476a7974965e149fce2d4", 
+        value: "74000000000000000"})
+}
